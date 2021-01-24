e24ef7617b5fde63e02f3b63d360785c0d43b05a
Claire Brown <claireconstans@msn.com>
2021-01-24 07:36:32 -0800

    reads csv data

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as csv from 'fast-csv';
+const fs = require('fs')
+const fastcsv = require('fast-csv')
+const csv = require('@fast-csv/parse')
 
 // Read Csv
-fs.createReadStream(path.resolve(__dirname, 'assets', 'parse.csv'))
-    .pipe(csv.parse({ headers: true }))
-    .on('error', error => console.error(error))
-    .on('data', row => console.log(row))
-    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
 
-    
+// interface CitiesCsvRow {
+//   id: string;
+//   cityName: string;
+//   country: string;
+//   transparancyScore: float;
+//   emissionsImpactScore: float;
+//   overallScore: float;
+// }
+
+// interface CitiesDetailsRow {
+//   id: number;
+//   cityName: string;
+//   country: string;
+//   region: string;
+//   reportsMade: integer;
+//   mostRecentYear: integer;
+//   usesStandardProtocol: boolean;
+//   GHGReported: integer;
+//   totalEmissions: float;
+//   perCaptiaEmissions: float;
+//   population: bigInt;
+//   trajectory: string;
+// }
+
+const stream = fs.createReadStream('resources/CDP_Community_GHG_Emissions_2016-20_Pixel_Cities.csv')
+
+let csvData = []
+
+csv.parseStream(stream)
+    .on('error', error => console.error(error))
+    .on('data', function(data) {
+      csvData.push(data)
+    })
+    .on('end', function() {
+      // remove the first line: header
+      csvData.shift()
+      console.log(csvData)
+    });
+
+
 // Append Table
