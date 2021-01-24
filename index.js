478c679f142804f99be00478d47bd5a48f3f7278
Claire Brown <claireconstans@msn.com>
2021-01-24 08:38:31 -0800

    data is a json object!

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-const fs = require('fs')
-const fastcsv = require('fast-csv')
-const csv = require('@fast-csv/parse')
+let csvToJson = require('convert-csv-to-json')
 
 // Read Csv
 
-// interface CitiesCsvRow {
-//   id: string;
-//   cityName: string;
-//   country: string;
-//   transparancyScore: float;
-//   emissionsImpactScore: float;
-//   overallScore: float;
-// }
+let data = csvToJson
+  .parseSubArray(',',',')
+  .fieldDelimiter(',')
+  .formatValueByType()
+  .getJsonFromCsv("resources/CDP_Community_GHG_Emissions_2016-20_Pixel_Cities.csv")
 
-// interface CitiesDetailsRow {
-//   id: number;
-//   cityName: string;
-//   country: string;
-//   region: string;
-//   reportsMade: integer;
-//   mostRecentYear: integer;
-//   usesStandardProtocol: boolean;
-//   GHGReported: integer;
-//   totalEmissions: float;
-//   perCaptiaEmissions: float;
-//   population: bigInt;
-//   trajectory: string;
-// }
-
-const stream = fs.createReadStream('resources/CDP_Community_GHG_Emissions_2016-20_Pixel_Cities.csv')
-
-let csvData = []
-
-csv.parseStream(stream)
-    .on('error', error => console.error(error))
-    .on('data', function(data) {
-      csvData.push(data)
-    })
-    .on('end', function() {
-      // remove the first line: header
-      csvData.shift()
-      console.log(csvData)
-    });
-
+console.log(data)
 
 // Append Table
