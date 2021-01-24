570cb50ea23093023d42317dbd79510c61441ad6
Claire Brown <claireconstans@msn.com>
2021-01-24 06:37:35 -0800

    reformatted column names

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+import * as fs from 'fs';
+import * as path from 'path';
+import * as csv from 'fast-csv';
 
 // Read Csv
+fs.createReadStream(path.resolve(__dirname, 'assets', 'parse.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => console.log(row))
+    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+
+    
 // Append Table
